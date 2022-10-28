794a54c39d003421ec2b0bf061e36ce5726e2594
benjaminf <benjamin.frazer.1997@gmail.com>
2022-10-28 11:45:10 +0100

    added front(),back() accessors and pop returns value

--- src/tsDeque.h
+++ src/tsDeque.h
@@ -6,31 +6,47 @@
 template <typename T>
 class TsDeque{
     public:
+        const T& front(){
+            const std::lock_guard<std::mutex> lock(mu);
+            return q.front();
+            }
+        const T& back(){
+            const std::lock_guard<std::mutex> lock(mu);
+            return q.back();
+        }
         T pop_front(){
             const std::lock_guard<std::mutex> lock(mu);
-            return q.pop_front();
+            auto t = std::move(q.front());
+            q.pop_front();
+            return t;
         };
         T pop_back(){
             const std::lock_guard<std::mutex> lock(mu);
-            return q.pop_back();
+            auto t = std::move(q.back());
+            q.pop_back();
+            return t;
         };
+        void push_back(T val){
+            const std::lock_guard<std::mutex> lock(mu);
+            q.push_back(val);
+        }
+        void push_front(T val){
+            const std::lock_guard<std::mutex> lock(mu);
+            q.push_front(val);
+        }
         template <typename Type>
         T at(Type i){
             const std::lock_guard<std::mutex> lock(mu);
             return q.at(i);
         };
+        const bool& empty(){
+            return q.empty();
+        };
     protected:
-        void testMeth();
         std::deque<T> q;
         std::mutex mu;
 
 };
 
-template <typename T>
-void TsDeque<T>::testMeth(){
-    q.pop_back();
-    q.pop_front();
-    q.at(1);
-}
 
 #endif // TSDEQUE_H_
